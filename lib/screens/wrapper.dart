caeceb34f107ff98bab4a423cad78d0b37249993
Pushkar Deshmukh <pushkard@markk.app>
2022-05-18 09:41:13 +0530

    Remove boilerplate code Add basic screen structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:demo_app/screens/home/authenticate/authenticate.dart';
+import 'package:demo_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return either Home or Authenticate widget
+    return Authenticate();
+  }
+}
